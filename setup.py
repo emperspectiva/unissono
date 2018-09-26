be32b794ea608268c6a44b8881605419f98a191c
Thiago Cardoso <thiagoncc@gmail.com>
2018-09-26 09:54:55 -0300

    adding skel

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="unissono",
+    description="tbd",
+    author="emperspectiva",
+    version="0.1",
+    packages=find_packages(),
+    test_requires=[
+        "pytest",
+    ],
+)
