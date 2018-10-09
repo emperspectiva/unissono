bc4f8446280506d592aa38fd09ab09ebbad1ed6a
Thiago Cardoso <thiagoncc@gmail.com>
2018-10-09 09:16:23 -0300

    Início do download dos microdados PNAD

---
+++ unissono/datasets/__init__.py
@@ -0,0 +1,43 @@
+import os
+import logging
+import urllib.request
+import zipfile
+from urllib.parse import urlparse
+from pathlib import Path
+from tqdm import tqdm
+
+DATASETS_DIR = Path.home() / ".unissono/data/"
+
+logger = logging.getLogger(__name__)
+
+
+def download(url, dest):
+    """ Download given url to dest.
+    """
+    dest.mkdir(parents=True, exist_ok=True)
+    print("created")
+    fname = os.path.basename(urlparse(url).path)
+    fname_abs = dest / fname
+
+    if fname_abs.exists():
+        logger.info("File already exists: %s" % (fname_abs))
+        return fname_abs
+
+    with fname_abs.open("wb") as o:
+        with urllib.request.urlopen(url) as f:
+            content_length = int(f.info()["Content-length"])
+            with tqdm(total=content_length) as pbar:
+                while True:
+                    buf = f.read(16*1024)
+                    if not buf:
+                        break
+                    o.write(buf)
+                    pbar.update(len(buf))
+
+    return fname_abs
+
+
+def extract_zip(fname, dest):
+    zip = zipfile.ZipFile(fname, "r")
+    zip.extractall(dest)
+    zip.close()
---
+++ unissono/datasets/pnad.py
@@ -0,0 +1,68 @@
+import logging
+import unissono.datasets
+import pandas as pd
+from tqdm import tqdm
+
+_PNAD_TRABALHO_2017 = "ftp://ftp.ibge.gov.br/Trabalho_e_Rendimento/Pesquisa_Nacional_por_Amostra_de_Domicilios_continua/Trimestral/Microdados/2017/Suplementos/Dados/PNADC_022017_educacao_20180816.zip" # noqa
+_PNAD_TRABALHO_2017_INPUT = "ftp://ftp.ibge.gov.br/Trabalho_e_Rendimento/Pesquisa_Nacional_por_Amostra_de_Domicilios_continua/Trimestral/Microdados/2017/Suplementos/Documentacao/Input_PNADC_trimestral_educacao_20180816.txt" # noqa
+
+logger = logging.getLogger(__name__)
+
+
+def download():
+    dest = unissono.datasets.DATASETS_DIR / "pnad_trabalho_2017"
+    logger.info("Downloading PNAD_TRABALHO_2017")
+    fname = unissono.datasets.download(_PNAD_TRABALHO_2017, dest)
+    logger.info("Downloading SAS input")
+    unissono.datasets.download(_PNAD_TRABALHO_2017_INPUT, dest)
+    logger.info("Extracting dataset files")
+    unissono.datasets.extract_zip(fname.absolute(), dest.absolute())
+
+
+def _parse_sas(fname):
+    data_dict = []
+    with fname.open("r") as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith("@"):
+                spt = line.split(maxsplit=3)
+                start = int(spt[0].strip("@"))-1  # 0 index
+                col = spt[1]
+                sz = int(spt[2].strip("$").strip("."))
+                doc = spt[3].strip("/*").strip("*/").strip()
+                data_dict.append([start, col, sz, doc])
+    return data_dict
+
+
+def _file_lines(fname):
+    lines = 0
+    with fname.open("r") as f:
+        buf_size = 1024*1024
+        buf = f.read(buf_size)
+        while buf:
+            lines += buf.count("\n")
+            buf = f.read(buf_size)
+    return lines
+
+
+def load():
+    dest = unissono.datasets.DATASETS_DIR / "pnad_trabalho_2017"
+    fname_input = dest / "Input_PNADC_trimestral_educacao_20180816.txt"
+    cols = _parse_sas(fname_input)
+    fname = dest / "PNADC_022017_educacao_20180815.txt"
+
+    columns = [x[1] for x in cols]
+    data_dict = {x[1]: x[3] for x in cols}
+    data = []
+
+    with fname.open("r") as f:
+        with tqdm(total=_file_lines(fname)) as pbar:
+            for line in f:
+                row = []
+                for start, col, sz, doc in cols:
+                    v = line[start:start+sz]
+                    row.append(v)
+                data.append(row)
+                pbar.update(1)
+    df = pd.DataFrame(data, columns=columns)
+    return df, data_dict
